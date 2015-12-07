f239dac3ed23cec81d289712fdbb228cb894869a
Anthony A. Walker <anthonyaustinwalker@gmail.com>
2015-12-07 01:42:04 +0000

    added 'pages' routes, controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root "welcome#home"
   get "about", to: "welcome#about"
+  get "pages/home", to: "pages#home"
+  get "pages/about", to: "pages#about"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
