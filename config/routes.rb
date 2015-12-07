213d14946e2f580e3b801e88e73b579d4dfb0709
Anthony A. Walker <anthonyaustinwalker@gmail.com>
2015-12-07 06:48:10 +0000

    create new template, action, route

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root "welcome#home"
-  get "about", to: "welcome#about"
-  get "pages/home", to: "pages#home"
-  get "pages/about", to: "pages#about"
+  root "pages#home"
+  get "about", to: "pages#about"
+  
+  resources :articles
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
